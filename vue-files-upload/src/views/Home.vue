52bd9c9443b47bea96da7c299773057434bf227e
Mil <maebelam@gmail.com>
2020-07-27 03:11:47 +0200

    Added vue-files-upload practice

---
+++ vue-files-upload/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <fileUpload></fileUpload>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import fileUpload from '@/components/fileUpload.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    fileUpload
+  }
+}
+</script>
